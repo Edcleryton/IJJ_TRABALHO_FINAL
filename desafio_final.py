ddfa43de705e2dc2f95a07fe3a8cc59567720b50
Edcleryton Silva <edcleryton.gabriel@gmail.com>
2025-04-25 19:59:51 -0300

    primeira requisição status code 200

--- desafio_final.py
+++ desafio_final.py
@@ -1,17 +1,7 @@
 import requests
-import json
-from datetime import datetime
 
-# URLs dos endpoints
-URL_CRIAR_USUARIO = "https://desafiopython.jogajuntoinstituto.org/api/users/"
-URL_LOGIN = "http://desafiopython.jogajuntoinstituto.org/api/users/login/"
+url = requests.get('https://desafiopython.jogajuntoinstituto.org/api/users')
 
-# Dados para criar usuário
-dados_criar_usuario = {
-    "username": "userteste",
-    "email": "teste@teste.com",
-    "password": "123456789",
-    "phone": "12345678901",
-    "address": "123 Main St, City, Country",
-    "cpf": "003.456.789-00"
-}
+print(url)
+
+
