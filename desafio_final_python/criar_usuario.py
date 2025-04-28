d4c9f776bebf2ea4582e6becaadcc0110a5648f2
Edcleryton Silva <edcleryton.gabriel@gmail.com>
2025-04-28 08:07:17 -0300

    :wrench: refactor: ajustar estrutura de diretórios e refinar lógica de login
    - Moveu arquivos de script para a pasta 'desafio_final'.
    - Atualizou o script 'fazer_login' para incluir tratamento de codificação UTF-8.
    - Adicionado salvamento da resposta JSON em 'resposta_login.json'.
    - Implementada mensagem de sucesso/falha no login com emojis para melhor visualização.

---
+++ desafio_final_python/criar_usuario.py
@@ -0,0 +1,28 @@
+import requests
+import sys
+sys.stdout.reconfigure(encoding='utf-8')
+# URL do endpoint para criar usuário
+url_criar_usuario = "https://desafiopython.jogajuntoinstituto.org/api/users/"
+
+# Dados do usuário
+dados_criar_usuario = {
+    "username": "Eddie_test",
+    "email": "edcton.teste@teste.com",
+    "password": "123456789",
+    "phone": "12345678901",
+    # "address": "Rua floriano peixoto 142, salvador, Brasil",
+    # "cpf": "003.456.789-00"
+}
+
+# Enviar POST
+resposta = requests.post(url_criar_usuario, json=dados_criar_usuario)
+
+# Verificar status e exibir resultado
+if resposta.status_code == 201:
+    print(f"\u2705 Usuário criado com sucesso!{resposta.status_code}")
+    print("Detalhes da resposta:", resposta.json())
+else:
+    print(f"\u274c Falha ao criar usuário. Status Code: {resposta.status_code}")
+    print("Resposta da API:", resposta.json())
+    exit()
+
